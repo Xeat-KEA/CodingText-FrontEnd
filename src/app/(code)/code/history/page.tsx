8c992b8fc6da3ddef07794235d1cf0733b308d5a
seorang42 <seorang42@naver.com>
2024-10-23 09:51:14 +0900

    CT-112 문제 목록, 문제 풀이 기록 페이지 API 연결

--- src/app/(code)/code/history/page.tsx
+++ src/app/(code)/code/history/page.tsx
@@ -2,23 +2,34 @@
 
 import { usePaginationStore } from "@/app/stores";
 import HistoryTopBar from "../../_components/HistoryTopBar";
-import { useEffect } from "react";
-import { DUMMY_CODE_HISTORY } from "../../_constants/constants";
+import { useEffect, useState } from "react";
 import HistoryCard from "../../_components/HistoryCard";
 import { useCheckToken } from "@/app/_hooks/useCheckToken";
+import api from "@/app/_api/config";
+import { History } from "../../_interfaces/interfaces";
 
 export default function CodeHistoryPage() {
   // 로그인 여부 확인
   const {} = useCheckToken(true);
 
-  const { setPage, setLastPage } = usePaginationStore();
+  const [data, setData] = useState<History[]>([]);
+
+  // 페이지네이션
+  const { page, setPage, setLastPage } = usePaginationStore();
+  // 첫 페이지 초기화
   useEffect(() => {
     setPage(1);
-    setLastPage(26);
-  }, []);
-
+    setLastPage(Math.ceil(data.length / 10));
+  }, [data]);
   // Page 변화 감지 후 문제 리스트 다시 GET 하는 과정 필요
 
+  // 프로토타입 API 코드 풀이 기록 GET
+  useEffect(() => {
+    api.get("history-list/1").then((res) => {
+      const sortedResult: History[] = res.data.data.reverse();
+      setData(sortedResult);
+    });
+  }, []);
   // 코드 필터 변화 감지 후 문제 리스트 다시 GET 하는 과정 필요
 
   return (
@@ -27,20 +38,19 @@ export default function CodeHistoryPage() {
       <HistoryTopBar />
       {/* 문제 */}
       <div className="w-full flex flex-col divide-y divide-border-1">
-        {DUMMY_CODE_HISTORY.map((el, index) => {
-          return (
-            <HistoryCard
-              key={index}
-              id={el.id}
-              title={el.title}
-              difficulty={el.difficulty}
-              participants={el.participants}
-              rate={el.rate}
-              hasSolved={el.hasSolved}
-              createdAt={el.createdAt}
-            />
-          );
-        })}
+        {data.map((el) => (
+          <HistoryCard
+            key={el.historyId}
+            historyId={el.historyId}
+            createdAt={el.createdAt}
+            title={el.title}
+            isCorrect={el.isCorrect}
+            registerStatus={el.registerStatus}
+            isAI={el.isAI}
+            codeId={el.codeId}
+            userId={el.userId}
+          />
+        ))}
       </div>
     </div>
   );
