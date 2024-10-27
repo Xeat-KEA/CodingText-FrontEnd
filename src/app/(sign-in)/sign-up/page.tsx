45589ed799e80633e0f5e53c4414cb123ca4bc2c
seorang42 <seorang42@naver.com>
2024-10-27 18:26:09 +0900

    CT-122 로그인/회원가입 페이지 리팩토링

--- src/app/(sign-in)/sign-up/page.tsx
+++ src/app/(sign-in)/sign-up/page.tsx
@@ -1,77 +1,17 @@
 "use client";
 
-import DropDown from "@/app/_components/DropDown";
-import { PROGRAMMING_LANGUAGES } from "@/app/_constants/constants";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import SmBackBtn from "../_components/SmBackBtn";
-import { useRouter } from "next/navigation";
-import ProfileImgSelection from "@/app/_components/ProfileImgSelection";
-import { SignUpForm } from "../_interfaces/interfaces";
+import SignInTitle from "../_components/SignInTitle";
+import SignUpFormContainer from "../_components/SignUpFormContainer";
 
 export default function SignUpPage() {
-  const router = useRouter();
-  const { register, handleSubmit, setValue } = useForm<SignUpForm>();
-  const [selectedLang, setSelectedLang] = useState("");
-  const [selectedProfileImg, setSelectedProfileImg] =
-    useState("/profileImg1.png");
-
-  const onValid = (data: SignUpForm) => {
-    // 데이터 post 및 validation 필요
-    console.log(data);
-
-    if (!data.lang) {
-      console.log("error");
-    } else {
-      router.push("/sign-up/done");
-    }
-  };
-
   return (
     <div className="sign-in-container">
-      <div className="flex flex-col gap-4 items-center relative">
-        <span className="sign-in-title">회원가입</span>
-        <span className="sign-in-content">
-          {"거의 다 끝났어요!\n당신의 정보를 입력해주세요"}
-        </span>
-        <SmBackBtn />
-      </div>
-      <form onSubmit={handleSubmit(onValid)} className="flex flex-col gap-8">
-        {/* 닉네임 입력 */}
-        <input
-          {...register("nickname", { required: true })}
-          className="sign-in-input"
-          placeholder="닉네임"
-          autoComplete="off"
-        />
-        {/* 언어 선택 */}
-        <div className="flex flex-col gap-2">
-          <span className="text-sm text-black">기본 프로그래밍 언어</span>
-          <DropDown
-            selection={selectedLang}
-            placeholder="언어를 선택해주세요"
-            list={PROGRAMMING_LANGUAGES}
-            onSelectionClick={(selected) => {
-              setSelectedLang(selected.content);
-              setValue("lang", selected.selection);
-            }}
-          />
-        </div>
-        {/* 프로필 사진 선택 */}
-        <div className="flex flex-col gap-2">
-          <span className="text-sm text-black">프로필 사진 선택</span>
-          <ProfileImgSelection
-            seletedImg={selectedProfileImg}
-            onSelectionClick={(selected) => {
-              setSelectedProfileImg(selected);
-              setValue("profileImg", selected);
-            }}
-          />
-        </div>
-        <button type="submit" className="btn-primary">
-          완료
-        </button>
-      </form>
+      <SignInTitle
+        title="회원가입"
+        content={"거의 다 끝났어요!\n당신의 정보를 입력해주세요"}
+        hasBackBtn
+      />
+      <SignUpFormContainer />
     </div>
   );
 }
--- src/app/(sign-in)/terms/page.tsx
+++ src/app/(sign-in)/terms/page.tsx
@@ -1,20 +1,8 @@
 "use client";
 
-import SmBackBtn from "../_components/SmBackBtn";
+import PolicyContainer from "../_components/PolicyContainer";
 import { TERMS } from "../_constants/constants";
 
 export default function TermsPage() {
-  return (
-    <div className="flex flex-col grow py-16 gap-8 w-full">
-      <div className="w-full flex justify-center relative">
-        <span className="sign-in-title">약관</span>
-        <SmBackBtn />
-      </div>
-      <div className="relative grow overflow-y-auto border border-border-2 rounded-2xl">
-        <span className="absolute text-black whitespace-pre-wrap px-4 py-3 text-xs">
-          {TERMS}
-        </span>
-      </div>
-    </div>
-  );
+  return <PolicyContainer title="약관" content={TERMS} />;
 }
