497ebcdb02bfbab8116e6487a6a6a42091f0057e
lamiiiii <115445005+lamiiiii@users.noreply.github.com>
2024-11-03 23:24:40 +0900

    CT-130 코드 리뷰 피드백 반영

--- src/app/(blog)/_components/BlindPostContainer.tsx
+++ src/app/(blog)/_components/BlindPostContainer.tsx
@@ -10,14 +10,14 @@ export default function BlindPostContainer() {
             <div className="max-w-800 min-h-screen flex flex-col items-center">
                 <div className="flex flex-col items-center mt-20">
                     <LogoIcon />
-                    <div className="h-32" />
+                    <div className="h-28" />
                     <SmBubbleIcon />
                 <span className="whitespace-pre-wrap text-2xl font-semibold text-black mt-2 mb-12">
                     {"블라인드 처리된 \n 게시물이에요"}
                 </span>
                 <button
                 onClick={() => router.back()}
-                className="w-4/5 h-[79px] border border-border-2 rounded-2xl bg-white text-xl text-black font-semibold"
+                className="w-4/5 py-6 border border-border-2 rounded-2xl bg-white text-xl text-black font-semibold"
                 >
                     이전 페이지로
                 </button>
