29c0a68f57ceadb301733a799c033eeb2c9ff5ac
seorang42 <seorang42@naver.com>
2024-11-12 16:18:16 +0900

    블로그 정보 초기화 기능 추가

--- src/app/(admin)/_components/UserDetailInfo.tsx
+++ src/app/(admin)/_components/UserDetailInfo.tsx
@@ -1,6 +1,7 @@
 import api from "@/app/_api/config";
 import ProfileImgContainer from "@/app/_components/ProfileImgContainer";
 import { useQuery } from "@tanstack/react-query";
+import DOMPurify from "isomorphic-dompurify";
 
 export default function UserDetailInfo({ userId }: { userId: number }) {
   const fetchData = async () => {
@@ -14,9 +15,12 @@ export default function UserDetailInfo({ userId }: { userId: number }) {
     nickname: "코딩텍스트",
     profileImg: "/profileImg3.png",
     status: "안녕하세요 코딩텍스트입니다.",
+    blogIntro: "<h2>Hello</h2>",
   };
 
-  const onReset = (type: "nickname" | "profileImg" | "status") => {
+  const onReset = (
+    type: "nickname" | "profileImg" | "status" | "blogIntro"
+  ) => {
     // API를 통해 사용자 정보 초기화 후 반환값 재설정 필요
     if (type === "nickname") {
     } else if (type === "profileImg") {
@@ -62,6 +66,24 @@ export default function UserDetailInfo({ userId }: { userId: number }) {
         <span className="edit-title">상태 메세지</span>
         <span className="edit-sm-content">{dummyData.status}</span>
         <button onClick={() => onReset("status")} className="edit-btn-primary">
+          초기화
+        </button>
+      </div>
+      {/* 블로그 소개글 */}
+      <div className="edit-container">
+        <span className="edit-title">블로그 소개글</span>
+        <div className="w-full h-[400px] overflow-y-auto border border-border-2 rounded-2xl px-6 py-4">
+          <div
+            className="prose"
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(dummyData.blogIntro),
+            }}
+          />
+        </div>
+        <button
+          onClick={() => onReset("blogIntro")}
+          className="edit-btn-primary"
+        >
           초기화
         </button>
       </div>
