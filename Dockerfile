10a6fb63031bc428d408de7221cf664a07f20e7f
seorang42 <seorang42@naver.com>
2024-12-11 16:35:07 +0900

    next.config.mjs 파일 수정4

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:22.12.0-alpine
+FROM node:22.12
 ENV TZ Asia/Seoul
 WORKDIR /usr/src/app
 COPY package*.json ./
--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ const bundleAnalyzer = withBundleAnalyzer({
 });
 
 const nextConfig = {
-  assetPrefix: "/.",
   // 임시로 모든 도메인에서 오는 이미지 허용 (이미지 저장 로직 구현 후 수정 필요)
   images: {
     remotePatterns: [
