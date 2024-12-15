5a2716b337e02fc68c94145bb76c5b5b1255eb8c
seorang42 <seorang42@naver.com>
2024-12-16 01:33:53 +0900

    1차 개발에서 제외된 관리자 기능 SideBar에서 제거

--- src/app/(admin)/_constants/constants.ts
+++ src/app/(admin)/_constants/constants.ts
@@ -6,9 +6,9 @@ export const ADMIN_TAB_LIST: ProfileTab[] = [
   { content: "문제 관리", url: "/admin/code" },
   { content: "신고 내역", url: "/admin/report" },
   { content: "등록 신청 문제 내역", url: "/admin/registered" },
-  { content: "알림", url: "/admin/push" },
+  // { content: "알림", url: "/admin/push" },
   { content: "공지사항", url: "/admin/notice" },
-  { content: "클라우드 사용량", url: "/admin/billing" },
+  // { content: "클라우드 사용량", url: "/admin/billing" },
 ];
 
 export const REPORT_TAP_LIST = ["블로그", "게시글", "댓글"];
